03a43544a830d0edb5d0e80fb65aea86c9ae7909
vuongducnghia <vnvuongducnghia@gmail.com>
2021-07-13 10:53:53 +0700

    vertical seekbar

---
+++ AndroidViewTestDemo/app/src/main/java/com/example/androidviewtestdemo/VerticalSeekBar.kt
@@ -0,0 +1,73 @@
+package com.example.androidviewtestdemo
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatSeekBar
+import androidx.core.content.ContextCompat
+
+/**
+ * Created by ${nghia.vuong} on 13,July,2021
+ */
+class VerticalSeekBar : AppCompatSeekBar {
+
+    constructor(context: Context) : super(context) {
+        initView(context)
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyle: Int
+    ) : super(context, attrs, defStyle) {
+        initView(context)
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
+    ) : super(context, attrs) {
+        initView(context)
+    }
+
+    private fun initView(context: Context) {
+        // Set Custom Progress Bar Drawable
+        progressDrawable = ContextCompat.getDrawable(context, R.drawable.vertical_progress_selector)
+        // Set Custom Thumb
+        thumb = ContextCompat.getDrawable(context, R.drawable.vertical_progress_thumb)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(h, w, oldh, oldw)
+    }
+
+    @Synchronized
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec)
+        setMeasuredDimension(measuredHeight, measuredWidth)
+    }
+
+    override fun onDraw(c: Canvas) {
+        c.rotate(-90f)
+        c.translate(-height.toFloat(), 0f)
+        super.onDraw(c)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!isEnabled) {
+            return false
+        }
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                progress = max - (max * event.y / height).toInt()
+                onSizeChanged(width, height, 0, 0)
+            }
+            MotionEvent.ACTION_CANCEL -> {
+            }
+        }
+        return true
+    }
+}
